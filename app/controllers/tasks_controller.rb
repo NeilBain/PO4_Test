c80fa722ea6ecc837758ccf1b85ac1c9963b42a1
NeilBain <git@realrails.co.uk>
2013-12-02 11:52:07 +0000

    Fixes #1 - Now creating nested tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :get_project
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # GET /projects/#/tasks
   # GET /projects/#/tasks.json
   def index
-#    @tasks = Task.all
     @tasks = @project.tasks
   end
 
@@ -16,7 +15,7 @@ class TasksController < ApplicationController
 
   # GET /projects/#/tasks/new
   def new
-    @task = Task.new
+    @task = @project.tasks.new
   end
 
   # GET /projects/#/tasks/1/edit
@@ -26,13 +25,13 @@ class TasksController < ApplicationController
   # POST /projects/#/tasks
   # POST /projects/#/tasks.json
   def create
-    @task = Task.new(task_params)
+    @task = @project.tasks.new(task_params)
     if @task.buffer == :false
-      :update_buffer
+      #:update_buffer
     end
     respond_to do |format|
       if @task.save
-        format.html { redirect_to project_task_url, notice: 'Task was successfully created.' }
+        format.html { redirect_to [@project, @task], notice: 'Task was successfully created.' }
         format.json { render action: 'show', status: :created, location: @task }
       else
         format.html { render action: 'new' }
@@ -45,11 +44,11 @@ class TasksController < ApplicationController
   # PATCH/PUT /projects/#/tasks/1.json
   def update
     if @task.buffer == false
-      :update_buffer
+      #:update_buffer
     end
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to project_tasks_url, notice: 'Task was successfully updated.' }
+        format.html { redirect_to [@project, @task], notice: 'Task was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -66,7 +65,7 @@ class TasksController < ApplicationController
       :update_buffer
     end
     respond_to do |format|
-      format.html { redirect_to tasks_url }
+      format.html { redirect_to project_tasks_path(@project) }
       format.json { head :no_content }
     end
   end
@@ -89,8 +88,8 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
-      @task = @project.task.find(params[:id])
-      @buffer = @project.task.find_by :buffer => true, :parent_id => @task.parent_id
+      @task = @project.tasks.find(params[:id])
+      @buffer = @project.tasks.find_by :buffer => true, :parent_id => @task.parent_id
     end
     
     def get_project
